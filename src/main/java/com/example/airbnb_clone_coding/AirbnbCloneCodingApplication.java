a4355960f92bb0e781dc5e880f5f0a19ee251db6
이현진 <hyun0325@sookmyung.ac.kr>
2025-04-07 18:06:38 +0900

    base file upload

---
+++ src/main/java/com/example/airbnb_clone_coding/AirbnbCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.example.airbnb_clone_coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneCodingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneCodingApplication.class, args);
+	}
+
+}
